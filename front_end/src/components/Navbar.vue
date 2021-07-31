a501bb1b3ddcc33a3738d4fa7f228358b099f029
Ducau Benjamin <ducaubenjamin@gmail.com>
2021-07-31 18:19:46 +0200

    mise en place du style des composants du mur

---
+++ front_end/src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<template>
+   <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+    <div class="container-fluid">
+        <span class="navbar-brand" ><img class="logo-img" src="../logos/icon-left-font-monochrome-white.svg" alt="logo groupomania"></span>
+        <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end " id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item mr-3">
+                    <router-link class="nav-link text-white " to="/profile">Mon profil</router-link>
+                </li>
+                <li class="nav-item ml-3">
+                    <a><router-link class="nav-link text-white " to="/login">Se déconnecter</router-link></a>
+                </li>
+            </ul>
+        </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default ({
+  name: 'Navbar',
+  data () {
+
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.logo-img{
+    width: 200px;
+    height: 70px;
+    @media (min-width: 630px){
+        width: 350px;
+    };
+    @media (min-width: 930px){
+        width: auto;
+        margin-left: 2rem;
+    };
+}
+#navbarNav{
+    @media (min-width: 990px){
+        margin-right: 2rem;
+    };
+}
+.nav-item{
+     @media (min-width: 990px){
+        margin-right: 2rem;
+    };
+}
+</style>
--- front_end/src/views/Home.vue
+++ front_end/src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar/>
+    <div class="container-fluid">
+       <h1 class=" bg-info text-dark">Bienvenue sur le forum</h1>
+    </div>
+    <main class="container d-flex justify-content-center">
+      <div class="card ">
+        <img src="../logos/curry.jpg" class="card-img-top" alt="">
+        <div class="card-body">
+          <h3 class="card-title">Titre de la description ici</h3>
+          <p class="h5 card-text">Some quick example text to build on the card title and make up the bulk of the card's content. It's very nice dont you think</p>
+        <div>
+          <p class="text-md-end">Posté par Fleury Michon</p>
+        </div>
+        </div>
+        <div class="d-flex">
+          <button class="w-50"></button>
+          <span class="w-50 bg-info"></span>
+        </div>
+        <div class="d-flex flex-column flex-lg-row justify-content-between">
+          <span>Rose Marie de La fuente a dit : </span>
+          <p class="h5">Waouh c'est de troip de la balle ce truc de fou!</p>
+        </div>
+        <p class="h5">1er commentaire ici</p>
+        <p>2ème commentaire ici</p>
+        <div class="input-group mb-3 p-2">
+          <input class="form-control" placeholder="Ajouter un commentaire" aria-label="zone d'ajout de commentaire" aria-describedby="bouton validation commentaire" type="text-area"/>
+          <button class="btn btn-outline-secondary comment-btn">Commenter</button>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home{
+  height: 100vh;
+  width: 100vw;
+  background-image: linear-gradient(to bottom right,#ffd7d7,rgb(224, 155, 215));
+}
+.side-section {
+  min-height: 30vh;
+  max-height: 90vh;
+}
+.post-section{
+  min-height: 30vh;
+  max-height: 90vh;
+}
+// .comment-btn{
+//   min-width: 30px;
+// }
+</style>
